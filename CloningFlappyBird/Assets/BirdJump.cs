917f12d07971e0699141943346906d4b51829e83
yangki1828 <107459857+yangki1828@users.noreply.github.com>
2022-07-05 17:06:36 +0900

    touch&pipeGenerator&Destory

--- CloningFlappyBird/Assets/BirdJump.cs
+++ CloningFlappyBird/Assets/BirdJump.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour
 {
-    // Rigidbody2D rb;
+    Rigidbody2D rb;
+    public float jumpPower;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            rb.velocity = Vector2.up * jumpPower;//(0,1)
+        }
     }
 }
