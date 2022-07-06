2ff3ac13fa28e5784ad764516c78393c79435aa8
yangki1828 <107459857+yangki1828@users.noreply.github.com>
2022-07-06 17:41:18 +0900

    Scoring by Trigger

---
+++ CloningFlappyBird/Assets/Score.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;//What is "static" doing?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = score.ToString();//str(score)
+    }
+}
